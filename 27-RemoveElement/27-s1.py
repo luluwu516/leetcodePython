3f301f078c4201475621103a048ccdd7ae69b569
Lulu Wu <sky110423@gmail.com>
2024-03-02 21:53:17 -0800

    27. Remove Element

---
+++ 27-RemoveElement/27-s1.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if not len(nums):
+            return 0
+
+        count = 0
+        for n in nums:
+            if n != val:
+                nums[count] = n
+                count += 1
+
+        return count
+
+
+def main():
+    # declaration and input
+    s = Solution()
+    nums = input("Enter values for a list separated by spaces: ").split()
+    nums = [int(val) for val in nums]
+    val = int(input("Enter the element to remove: "))
+    expected_nums = []
+
+    # processing
+    k = s.removeElement(nums, val)
+    nums = nums[:k]
+
+    # output
+    print("\nResult:", k, "duplicates and nums =", nums)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 27-RemoveElement/27-s2.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if not len(nums):
+            return 0
+
+        while val in nums:
+            nums.remove(val)
+
+        return len(nums)
+
+
+def main():
+    # declaration and input
+    s = Solution()
+    nums = input("Enter values for a list separated by spaces: ").split()
+    nums = [int(val) for val in nums]
+    val = int(input("Enter the element to remove: "))
+    expected_nums = []
+
+    # processing
+    k = s.removeElement(nums, val)
+    nums = nums[:k]
+
+    # output
+    print("\nResult:", k, "duplicates and nums =", nums)
+
+
+if __name__ == "__main__":
+    main()
