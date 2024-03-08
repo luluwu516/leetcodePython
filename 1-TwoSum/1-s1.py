bca6aeeef8fdf144dedff9ae045948381041f78a
Lulu Wu <sky110423@gmail.com>
2024-03-07 19:57:19 -0800

    Improve and modify to Python 3

--- 1-TwoSum/1-s1.py
+++ 1-TwoSum/1-s1.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         size = len(nums)
         for i in range(size - 1):
+            second = nums[i] - target
             for j in range(i + 1, size):
-                if nums[i] + nums[j] == target:
+                if nums[j] == second:
                     return {i, j}
 
         return {}
