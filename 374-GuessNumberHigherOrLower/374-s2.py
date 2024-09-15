bf6b8fed707e56245cc4f3b38c4031459b81dd26
Lulu Wu <sky110423@gmail.com>
2024-09-14 21:27:18 -0700

    374. Guess Number Higher or Lower

---
+++ 374-GuessNumberHigherOrLower/374-s2.py
@@ -0,0 +1,54 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+
+class Solution:
+    def Solution(self, pick: int = 0):
+        self.__pick = pick
+
+    def setPick(self, pick_num: int) -> None:
+        self.__pick = pick_num
+
+    def guess(self, guess_num: int) -> int:
+        if guess_num == self.__pick:
+            return 0
+        elif guess_num > self.__pick:
+            return -1
+        else:
+            return 1
+
+    def guessNumber(self, n: int) -> int:
+        return self.guessNumberRec(1, n)
+
+    def guessNumberRec(self, low: int, high: int) -> int:
+        mid = low + (high - low) // 2
+        res = self.guess(mid)
+        if res == 0:
+            return mid
+        elif res > 0:
+            return self.guessNumberRec(mid + 1, high)
+        else:
+            return self.guessNumberRec(low, mid - 1)
+
+
+def main():
+    # declaration and input
+    s = Solution()
+    num = int(input("\nEnter the pick: "))
+    s.setPick(num)
+    num = int(input("Enter an integer: "))
+
+    # processing
+    res = s.guessNumber(num)
+
+    # output
+    print("\nResult:")
+    print(f"The pick number is {res}\n")
+
+
+if __name__ == "__main__":
+    main()
